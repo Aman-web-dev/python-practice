d3ab697c3db16b7d40c3292d742efe7a4a7e8a1d
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-05-15 20:33:03 +0530

    Updates

---
+++ RAG_bot.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+
+# train the AI with Business data data using vector embedddings 
+# use chatgpt to convert the business data in vector data
+# save vector data inside the pinecone DB 
+# ask for user query 
+# change user query into the vector add additional prompts
+# use the saved vector data and filter out the appropiate answer and return it to the user
